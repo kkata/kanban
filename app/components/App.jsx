ff61d750d7936ceec0eb4f03ebb30315dc69064f
kkata <redprin900@yahoo.co.jp>
2016-04-23 15:54:52 +0900

    Implementing a Basic Note Application

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,69 @@
+import uuid from 'node-uuid';
 import React from 'react';
-import Note from './Note.jsx';
+import Notes from './Notes.jsx';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpak'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
+  }
   render() {
-    return <Note />;
+    const notes = this.state.notes;
+    return (
+      <div>
+        <button className="add-note" onClick={this.addNote}>+</button>
+        <Notes notes={notes}
+        onEdit={this.editNote}
+        onDelete={this.deleteNote}
+        />
+      </div>
+    );
+  }
+
+  deleteNote = (id,e) => {
+    e.stopPropagation();
+
+    this.setState({
+      notes: this.state.notes.filter(note => note.id !== id)
+    });
+  };
+  addNote = () => {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
+    })
+  };
+  editNote = (id,task) => {
+    if(!task.trim()) {
+      return;
+    }
+
+    const notes = this.state.notes.map(note => {
+      if(note.id === id && task) {
+        note.task = task;
+      }
+
+      return note;
+    });
+
+    this.setState({notes});
   }
 }
